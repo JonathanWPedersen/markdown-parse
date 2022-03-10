1a854b20812fa5a4eb7366a43c3f1033a1668a5f
JonathanWPedersen <97184870+JonathanWPedersen@users.noreply.github.com>
2022-03-10 11:39:30 -0800

    Update TryCommonMark.java

--- TryCommonMark.java
+++ TryCommonMark.java
@@ -9,6 +9,6 @@ class TryCommonMark
         Parser parser = Parser.builder().build();
         Node document = parser.parse("This is *Sparta*");
         HtmlRenderer renderer = HtmlRenderer.builder().build();
-        System.out.println(renderer.render(document);  // "<p>This is <em>Sparta</em></p>\n"
+        System.out.println(renderer.render(document));  // "<p>This is <em>Sparta</em></p>\n"
     }
 }
